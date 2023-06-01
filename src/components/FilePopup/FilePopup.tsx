2d031a91a1a7fc638408b016efefbf2ddd48adee
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-06-02 00:08:41 +0530

    input field

---
+++ src/components/FilePopup/FilePopup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilePopup = () => {
+  return (
+    <div>FilePopup</div>
+  )
+}
+
+export default FilePopup
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Input.css'
-import Paperclip from "../../icons/Paperclip.svg"
-import Send from "../../icons/Send.svg"
+import React from "react";
+import "./Input.css";
+import Paperclip from "../../icons/Paperclip.svg";
+import Send from "../../icons/Send.svg";
 
 const Input = () => {
   return (
     <div className="input-block">
-        <div className='input-box'>
+      <div className="input-box">
         <input type="text" placeholder="Reply to @Rohit Yadav" />
-        <img src={Paperclip} alt='paperclip'/>
-        <img src={Send} alt='send'/>
+        <div className="input-block-icons">
+          <img src={Paperclip} alt="paperclip" />
+          <img src={Send} alt="send" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
